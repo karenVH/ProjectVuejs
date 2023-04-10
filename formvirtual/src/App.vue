e57fd6eb25cf9d6b7cb4ad0290b1f3ac6e582c87
karenVH <38914172+karenVH@users.noreply.github.com>
2023-04-09 20:10:48 -0500

    Creación de formulario

--- formvirtual/src/App.vue
+++ formvirtual/src/App.vue
@@ -1,50 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+    <h1>REGISTRO</h1>
+    <TheWelcome />
 
-    <div class="wrapper">
-      <HelloWorld msg="formulario" />
-    </div>
-  </header>
-
-  <main>
-    <h1 class="text-3xl font-bold underline ">
-    Hello world!
-  </h1>
-    <!-- <TheWelcome /> -->
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+h1{
+color: #fad819;
+font-family: 'Montserrat', sans-serif;
 }
 </style>
